828540f753b95b4f6058a5ba1a06063ffbd4ad79
gnujesus <jotamartinezd@gmail.com>
2025-02-03 10:34:20 -0400

    finished count_letters exercise

--- c/exercises/count_letters_deep_seek.c
+++ c/exercises/count_letters_deep_seek.c
@@ -4,53 +4,42 @@
 void deleteChar(char *str, int index) {
   int length = strlen(str);
 
-  if(length == 1){
-    str[0] = '\0';
-  }
-
   if (index < 0 || index > length) {
-    printf("index out of bounds");
+    printf("INDEX OUT OF BOUNDS");
     return;
   }
 
   for (int i = index; i < length - 1; i++) {
-    str[i] = str[i + 1];
+    str[i] = str[i + i];
   }
 
   str[length - 1] = '\0';
 }
 
 int main() {
-  char str[] = "lllooooo";
-  printf("String to count: %s \n\n", str);
-
+  char str[] = "parangarigutilimicuaro";
   int length = strlen(str);
 
   while (length > 0) {
     char buffer = str[0];
     int count = 0;
 
-    for (int i = 0; i <= length;) {
-      printf("Value of i: %d \n\n", i);
-      if (buffer == str[i]) {
+    for (int j = 0; j < length;) {
+      if (buffer == str[j]) {
         count++;
+        deleteChar(str, j);
         length--;
-        deleteChar(str, i);
       } else {
-        i++;
+        j++;
       }
     }
 
-    printf("%c ----> ", buffer);
-    for (int i = 0; i < count; i++) {
+    printf("%c --> ", buffer);
+    for (int k = 0; k < count; k++) {
       printf("* ");
     }
     printf("\n");
-
-    length--;
   }
-
-  printf("\n");
 
   return 0;
 }
