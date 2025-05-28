ed9add6c73b04a89a043eb865b2458269e1dad5b
juandejesusmartinez <juan.martinezd@intec.edu.do>
2025-05-28 16:19:06 -0400

    learning

---
+++ c_sharp/multithreading/BasicSyntax/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text.Encodings.Web;
+
+void WriteThreadId()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(50);
+    }
+}
+
+Thread thread1 = new Thread(WriteThreadId);
+thread1.Start();
+
+Thread thread2 = new Thread(WriteThreadId);
+thread2.Start();
+
+WriteThreadId();
+
+
