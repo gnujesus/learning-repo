ee7019dfbebd2b825ecbbe414da9525ec705f188
juandejesusmartinez <juan.martinezd@intec.edu.do>
2025-06-02 14:53:50 -0400

    learning parallelism and made web server

---
+++ c_sharp/multithreading/DivideAndConquer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+int[] numbersArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var stopWatch = Stopwatch.StartNew();
+
+int numberOfSegments = 4; 
+int segmentLength = numbersArray.Length / numberOfSegments; // 2.5, truncated to 2
+
+// --
+
+int SumSegment(int start, int end)
+{
+    int segmentSum = 0;
+    for (int i = start; i < end; i++)
+    {
+        Thread.Sleep(100);
+        segmentSum += numbersArray[i];
+    }
+
+    return segmentSum;
+}
+
+var tasks = new Task<int>[]
+{
+    Task.Run(() => SumSegment(0, segmentLength)),
+    Task.Run(() => SumSegment(segmentLength, 2 * segmentLength)),
+    Task.Run(() => SumSegment(2 * segmentLength,  3 * segmentLength)),
+    Task.Run(() => SumSegment(3 * segmentLength,  numbersArray.Length)) // Handle remaining operations (MORE EXPENSIVE)
+};
+
+var results = await Task.WhenAll(tasks);
+var total = results.Sum();
+
+// --
+
+stopWatch.Stop();
+
+Console.WriteLine($"Elapsed: {stopWatch.Elapsed}");
+
+Console.WriteLine($"Sum: {total}");
---
+++ c_sharp/multithreading/WebServerSimulation/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Server is running. Type 'exit' to exit.");
+
+// Q
+var requestQ = new Queue<Task>();
+
+while (true)
+{
+    // Each input introduced is a request
+    string? input = Console.ReadLine();
+    
+    if (input?.ToLower() == "exit")
+    {
+        break;
+    }
+    
+    RequestHandler(input, requestQ);
+    RequestMonitor(requestQ);
+}
+
+static void RequestHandler(string? input, Queue<Task> q)
+{
+    q.Enqueue(Task.Run(() =>  ProcessInput(input) ));
+}
+
+static void RequestMonitor(Queue<Task> q)
+{
+    Console.WriteLine($"Queue Size: {q.Count}\nProcessing: {1}");
+
+    Task.Run(() => Console.WriteLine("Hello World"));
+}
+
+// Request Processor
+static void ProcessInput(string? input)
+{
+    // Simulate heavy task, long processing time
+    Thread.Sleep(5000);
+    Console.WriteLine($"Processed input: {input}");
+}
