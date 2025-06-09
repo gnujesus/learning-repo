c54c7a07661d2fcdc1fbaa8744202aeb75539c50
gnujesus <jotamartinezd@gmail.com>
2025-06-09 16:46:55 -0400

    learnt some things

---
+++ go/boot.dev/update-users/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type Membership struct {
+	Type             string
+	MessageCharLimit int
+}
+
+type User struct {
+	Name string
+	Membership
+}
+
+func newUser(name string, membershipType string) User {
+	charLimit := 0
+
+	if membershipType == "premium" {
+		charLimit = 1000
+	} else {
+		charLimit = 100
+	}
+
+	user := User{
+		Name: name,
+		Membership: Membership{
+			Type:             membershipType,
+			MessageCharLimit: charLimit,
+		},
+	}
+
+	return user
+}
+
+func (u User) sendMessage(message string, messageLength int) (string, bool) {
+	if messageLength <= u.Membership.MessageCharLimit {
+		return message, true
+	} else {
+		return "", false
+	}
+}
+
+func main() {
+	jesus := newUser("Jesus", "")
+
+	fmt.Println(jesus)
+
+	message := "Hello World"
+}
